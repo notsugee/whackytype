a71ee7ca7022acc3d058d65b37a26c79f54e9409
notsugee <p.sujith303@gmail.com>
2024-10-27 13:13:35 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,71 @@
-import Image from "next/image";
+"use client";
+import { Roboto_Mono } from "next/font/google";
+import { useState } from "react";
+
+const roboto_mono = Roboto_Mono({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const starterText = "Lorem Ipsum is simply dummy Ipsum.";
+  const [startTime, setStartTime] = useState<number | undefined>(undefined);
+  const [score, setScore] = useState(0);
+  const [words, setWords] = useState(0);
+  const [text, setText] = useState({
+    default: "",
+    highlighted: "",
+  });
+  const wpm = startTime
+    ? (words / ((new Date().getTime() - startTime) / 60000)).toFixed(0)
+    : "0";
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  return (
+    <>
+      <div
+        className={`${roboto_mono.className} mx-auto max-w-3xl relative top-28 items-center justify-items-center`}
+      >
+        <p>score: {score}</p>
+        <p>words: {words}</p>
+        <p>WPM: {wpm}</p>
+        {score === 0 ? (
+          <p className="text-gray-500 z-30">{starterText}</p>
+        ) : (
+          <div>
+            <p className="text-white inline">{text.highlighted}</p>
+            <p className="text-gray-500 inline">{text.default}</p>
+          </div>
+        )}
+        <div className="flex justify-center">
+          <input
+            className="absolute text-black w-full h-full top-0 z-0 opacity-0"
+            onChange={() => {
+              if (!startTime) {
+                setStartTime(new Date().getTime());
+              }
+              const newScore = score + 1;
+              setScore(newScore);
+
+              if (starterText.charAt(newScore) === " ") {
+                setWords(words + 1);
+              }
+
+              setText({
+                highlighted: starterText.slice(0, newScore),
+                default: starterText.slice(newScore, starterText.length),
+              });
+
+              if (newScore === starterText.length) {
+                setStartTime(undefined);
+                setScore(0);
+                setWords(0);
+                setText({
+                  highlighted: "",
+                  default: "",
+                });
+                alert("Your wpm is " + wpm);
+              }
+            }}
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </>
   );
 }
